ec360979edfbe3b679db7006459086ec72f5e770
Mike Kim <mk.kansas@gmail.com>
2024-02-19 22:49:37 +0900

    Popup Function javascript fix Try #4

--- assets/js/newpop.js
+++ assets/js/newpop.js
@@ -15,6 +15,7 @@ function openModal(modalId) {
 function closeModal(modalId) {
   var modal = document.getElementById(modalId);
   modal.style.display = 'none';
+  localStorage.setItem(modalId + '-lastClosed', new Date().toDateString());
 }
 
 // Function to set no show preference
@@ -22,19 +23,33 @@ function setNoShow(modalId) {
   var checkbox = document.getElementById('noShow' + modalId.slice(-1));
   localStorage.setItem(modalId + '-noShow', checkbox.checked);
   if (checkbox.checked) {
-    localStorage.setItem(modalId + '-lastClosed', new Date().toDateString());
+    closeModal(modalId);
   }
 }
 
 // Event listener to close modals when clicking outside of them
 window.onclick = function(event) {
-  if (event.target.classList.contains('modal')) {
-    event.target.style.display = 'none';
+  if (event.target.classList.contains('modal-close') || event.target.classList.contains('modal')) {
+    closeModal(event.target.closest('.modal').id);
   }
 }
+
+// Event listener for the checkbox to not show the modal again
+document.querySelectorAll('.no-show-checkbox').forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    setNoShow(this.dataset.modalId);
+  });
+});
 
 // Initialization to open modals
 window.onload = function() {
   openModal('popup1');
   openModal('popup2');
 }
+
+// Close button functionality
+document.querySelectorAll('.close-modal').forEach(function(button) {
+  button.addEventListener('click', function() {
+    closeModal(this.dataset.modalId);
+  });
+});
